9eacd3c8a9d73adfd938f451361eb7b2e62564cd
douglasojeda <douglasaojeda@gmail.com>
2025-11-27 17:29:13 -0500

    Initial Spring Boot structure.

---
+++ calllog/src/main/java/com/douglasojeda/calllog/CalllogApplication.java
@@ -0,0 +1,13 @@
+package com.douglasojeda.calllog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalllogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalllogApplication.class, args);
+	}
+
+}
---
+++ calllog/src/test/java/com/douglasojeda/calllog/CalllogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglasojeda.calllog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalllogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
