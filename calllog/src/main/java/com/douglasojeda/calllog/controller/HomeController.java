edcb1614ace08be164867a4ee8eb5302d657f8f5
douglasojeda <douglasaojeda@gmail.com>
2025-11-27 18:18:38 -0500

    Initial Controller Structure

---
+++ calllog/src/main/java/com/douglasojeda/calllog/controller/HomeController.java
@@ -0,0 +1,5 @@
+package com.douglasojeda.calllog.controller;
+
+public class HomeController {
+
+}
---
+++ calllog/src/main/java/com/douglasojeda/calllog/controller/PhoneNumberController.java
@@ -0,0 +1,7 @@
+package com.douglasojeda.calllog.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PhoneNumberController {
+}
