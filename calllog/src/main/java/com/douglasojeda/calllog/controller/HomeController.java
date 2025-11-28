ee771ff0eb4348cadd2572aac4011f7b2bbdceab
douglasojeda <douglasaojeda@gmail.com>
2025-11-28 16:32:53 -0500

    Fixed some issues in application.properties

--- calllog/src/main/java/com/douglasojeda/calllog/controller/HomeController.java
+++ calllog/src/main/java/com/douglasojeda/calllog/controller/HomeController.java
@@ -1,5 +1,14 @@
 package com.douglasojeda.calllog.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World";
+    }
 
 }
